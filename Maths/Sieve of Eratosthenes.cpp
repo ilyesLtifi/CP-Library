af194f132d9e7befc87088b92a5c45ef334c7c61
Ilyes <ilyes.glays@gmail.com>
2021-01-15 00:45:40 +0100

    Update Sieve of Eratosthenes.cpp

--- Maths/Sieve of Eratosthenes.cpp
+++ Maths/Sieve of Eratosthenes.cpp
@@ -1,12 +1,16 @@
 /**
-* Computes all prime numbers <= X in O(X*log(log(X))
+* Computes all prime numbers <= X in O(X)
 * In this implementation I'm using to compute something more; spf[i] is the smallest-prime-factor of i.
 * 
 * The phi-function(Euler's totient function) is also computed using the sieve. Read about it here: cp-algorithms.com/algebra/phi-function.html
 */
 
-for(int i=1; i<=X; ++i) spf[i] = i;
-for(int i=2; i<=X; ++i) if(spf[i] == i) for(int j=i+i; j<=X; j+=i) spf[j] = min(spf[j], i);
+int primeCnt = 0;
+for(int i=2; i<=X; ++i) spf[i] = 0;
+for(int i=2; i<=X; ++i) {
+    if(!spf[i]) primes[++primeCnt] = i, spf[i] = i;
+    for(int j=1; j<=primeCnt && 1LL*i*primes[j]<X && primes[j]<=spf[i]; j++) spf[i*primes[j]] = primes[j];
+}
 
 phi[1] = 1;
 for(int i=2; i<=X; i++) phi[i] = (spf[i] == spf[i/spf[i]]) ? phi[i/spf[i]]*spf[i] : phi[i/spf[i]]*(spf[i]-1);
