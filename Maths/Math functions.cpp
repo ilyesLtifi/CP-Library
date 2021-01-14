637951a99005da34759c98f9dedd4ce305c0895e
Ilyes <ilyes.glays@gmail.com>
2021-01-15 00:45:18 +0100

    Update Math functions.cpp

--- Maths/Math functions.cpp
+++ Maths/Math functions.cpp
@@ -2,14 +2,13 @@
 * Some Math Helper functions that compute modulo addition, multiplication, inverse etc..
 */
 
-LL MOD = 0;
-inline LL add(const LL& x, const LL& y, const LL& mod) { LL z=x+y; return (z>=mod+mod)?z%mod:((z>=mod)?z-mod:((z<0)?z+mod:z)); }
-inline LL mul(const LL& x, const LL& y, const LL& mod) { return (x*y)%mod; }
-inline LL power(const LL& x, const LL& n, const LL& mod){if(n==0)return 1;LL y=power(x, n>>1, mod);y=mul(y,y,mod);return (n&1)?mul(y,x,mod):y;}
+inline LL add(const LL& x, const LL& y) { LL z=x+y; return (z>=MOD+MOD)?z%MOD:((z>=MOD)?z-MOD:((z<0)?z+MOD:z)); }
+inline LL mul(const LL& x, const LL& y) { return (x*y)%MOD; }
+inline LL power(const LL& x, const LL& n){if(n==0)return 1;LL y=power(x, n>>1);y=mul(y,y);return (n&1)?mul(y,x):y;}
 inline LL _power(const LL& x, const LL& n){if(n==0)return 1;LL y=_power(x, n>>1);y=y*y;return (n&1)?y*x:y;}
 LL _fact[SIZE+2] = {0}; inline LL fact(const LL& x){ return (x==0 ? 1 : (_fact[x]!=0 ? _fact[x] : _fact[x]=mul(x, fact(x-1)))); }
 LL _invfact[SIZE+2] = {0}; inline LL invfact(const LL& x){ return (x==0 ? 1 : (_invfact[x]!=0 ? _invfact[x] : _invfact[x]=power(fact(x), MOD-2))); }
-inline LL inv(const LL& x, const LL& mod){ return power(x, mod-2, mod); }
+inline LL inv(const LL& x){ return power(x, MOD-2); }
 inline LL cnk(const LL& n, const LL& k){ return (n<k)?0:mul(fact(n),inv(mul(fact(k),fact(n-k)))); }
 
 
